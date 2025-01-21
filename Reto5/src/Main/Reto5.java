eaea5bdbcb55c35253737dc9d9b0e10dff7007f5
dPiazuelo <DAM1@aula11-pc27>
2025-01-21 09:05:56 +0100

    commit queja main

--- Reto5/src/Main/Reto5.java
+++ Reto5/src/Main/Reto5.java
@@ -13,78 +13,80 @@ import Clases.Tienda;
 public class Reto5 {
 
 	public static void main(String[] args) {
-	Random r=new Random();
-	Scanner sc=new Scanner(System.in);
-	List<Empleado> listaEmpleado=new ArrayList<Empleado>();
-	List<Tienda> listaTienda=new ArrayList<Tienda>();
-	for (int i = 1; i < 21; i++) {
-		Empleado empleado=new Empleado("nombre"+i,r.nextInt(1,21),r.nextDouble(1.00,2000.00));
-		listaEmpleado.add(empleado);
-	}
-	for(int i=1;i<6;i++) {
-		Tienda tienda =new Tienda(i,"tienda"+i,listaEmpleado.get(r.nextInt(0,20)));
-		tienda.anadirProducto(Util.Funciones.dimeEntero("Introduzca el numero de productos", sc), r);
-		listaTienda.add(tienda);
-	}
-	Cliente cliente=new Cliente();
-	
-	int opcion=6;
-	do {
-		System.out.println("1.Ver tiendas");
-		System.out.println("2.Comprar");
-		System.out.println("3.Mostrar compras");
-		System.out.println("4.Realizar una queja");
-		System.out.println("5.Salir");
-		opcion=Util.Funciones.dimeEntero("Elige una opcion", sc);
-		if(opcion==5) {
-			break;
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+		List<Empleado> listaEmpleado = new ArrayList<Empleado>();
+		List<Tienda> listaTienda = new ArrayList<Tienda>();
+		for (int i = 1; i < 21; i++) {
+			Empleado empleado = new Empleado("nombre" + i, r.nextInt(1, 21), r.nextDouble(1.00, 2000.00));
+			listaEmpleado.add(empleado);
 		}
-		if(opcion==1) {
-			verTiendas(listaTienda);
+		for (int i = 1; i < 6; i++) {
+			Tienda tienda = new Tienda(i, "tienda" + i, listaEmpleado.get(r.nextInt(0, 20)));
+			tienda.anadirProducto(Util.Funciones.dimeEntero("Introduzca el numero de productos", sc), r);
+			listaTienda.add(tienda);
 		}
-		if(opcion==2) {
-			comprar(sc,listaTienda,cliente,r);
-		}
-		if(opcion==3) {
-			break;
-		}
-		if(opcion==4) {
-			break;
-		}
-	}while(true);
-	
-	}
-	
-	public static  void verTiendas(List<Tienda> listaTienda) {
-		for (int i = 0; i < listaTienda.size(); i++) {
-			
-			System.out.println(listaTienda.get(i).toString());  
-		}
-	}
-	public static void comprar(Scanner sc, List<Tienda> listaTienda,Cliente cliente,Random r) {
-		Tienda tienda=pedirTienda(sc,listaTienda,r);
-		Producto producto=tienda.getProductos().get(r.nextInt(0,tienda.getProductos().size()));
-		tienda.quitarProducto(producto,r);
-		cliente.comprarProducto(tienda,producto);
-	} 
-	public static  Tienda pedirTienda(Scanner sc, List<Tienda> listaTienda,Random r) {
-		verTiendas(listaTienda);
+		Cliente cliente = new Cliente();
+
+		int opcion = 6;
 		do {
-		int eleccion=Util.Funciones.dimeEntero("Elige una tienda", sc);
-		for(int i=0; i<listaTienda.size();i++) {
-			if(eleccion==listaTienda.get(i).getId_tienda()) {
-				return listaTienda.get(i);
+			System.out.println("1.Ver tiendas");
+			System.out.println("2.Comprar");
+			System.out.println("3.Mostrar compras");
+			System.out.println("4.Realizar una queja");
+			System.out.println("5.Salir");
+			opcion = Util.Funciones.dimeEntero("Elige una opcion", sc);
+			if (opcion == 5) {
+				break;
 			}
-		}
-		}while(true);
-		
-	}
-	
-	public static void Queja(Scanner sc, List<Tienda> listaTienda,Random r, List<Empleado> listaEmpleado) {
-		Tienda t1 = Reto5.pedirTienda(sc, listaTienda, r);
-		t1.setGerente(listaEmpleado.get(r.nextInt(0,20)));
-		 
+			if (opcion == 1) {
+				verTiendas(listaTienda);
+			}
+			if (opcion == 2) {
+				comprar(sc, listaTienda, cliente, r);
+				
+			}
+			if (opcion == 3) {
+				break;
+			}
+			if (opcion == 4) {
+				queja(sc,listaTienda,r,listaEmpleado);
+			}
+		} while (true);
+
 	}
 
+	public static void verTiendas(List<Tienda> listaTienda) {
+		for (int i = 0; i < listaTienda.size(); i++) {
+
+			System.out.println(listaTienda.get(i).toString());
+		}
+	}
+
+	public static void comprar(Scanner sc, List<Tienda> listaTienda, Cliente cliente, Random r) {
+		Tienda tienda = pedirTienda(sc, listaTienda, r);
+		Producto producto = tienda.getProductos().get(r.nextInt(0, tienda.getProductos().size()));
+		tienda.quitarProducto(producto, r);
+		cliente.comprarProducto(tienda, producto);
+	}
+
+	public static Tienda pedirTienda(Scanner sc, List<Tienda> listaTienda, Random r) {
+		verTiendas(listaTienda);
+		do {
+			int eleccion = Util.Funciones.dimeEntero("Elige una tienda", sc);
+			for (int i = 0; i < listaTienda.size(); i++) {
+				if (eleccion == listaTienda.get(i).getId_tienda()) {
+					return listaTienda.get(i);
+				}
+			}
+		} while (true);
+
+	}
+
+	public static void queja(Scanner sc, List<Tienda> listaTienda, Random r, List<Empleado> listaEmpleado) {
+		Tienda t1 = Reto5.pedirTienda(sc, listaTienda, r);
+		t1.setGerente(listaEmpleado.get(r.nextInt(0, 20)));
+
+	}
 
 }
